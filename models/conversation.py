f945206179e4e759be9895016fa3f6f5244d7ef8
Wellington Silverio <wellington.silverio@synvia.com>
2019-05-30 17:13:24 -0300

    <refator> separated in functions

--- models/conversation.py
+++ models/conversation.py
@@ -52,15 +52,8 @@ class Conversations:
         self.mongo = Mongo().init()
 
     def newAnswer(self, answer):
-        algo = client.algo('translation/YandexTranslate/0.1.2')
-        translation = algo.pipe({
-            "to":"en",
-            "from":"pt",
-            "text": answer
-            }).result[0]
-
-        algo = client.algo('nlp/AutoTag/1.0.1')
-        tags = algo.pipe(translation).result
+        translation = self.translateAnswer(answer)
+        tags = self.getTags(translation)
 
         check = False
         bestReply = { "rating": 0 }
@@ -68,10 +61,7 @@ class Conversations:
         replies = []
 
         for __tag in tags:
-            _find = {}
-            _find["tags"] = {}
-            _find["tags"]["$all"] = []
-            _find["tags"]["$all"].append(__tag)
+            _find = self.getFind(__tag)
             _replies = self.mongo["dev"]["conversations"].find(_find)
             for _reply in _replies:
                 replies.append(_reply)
@@ -89,6 +79,26 @@ class Conversations:
                 return self.registerDbAndReturnNotReply(answer, translation, tags)
         else:
             return self.registerDbAndReturnNotReply(answer, translation, tags)
+
+    def translateAnswer(self, answer, textLanguageFrom="pt"):
+        algo = client.algo('translation/YandexTranslate/0.1.2')
+        return algo.pipe({
+            "to":"en",
+            "from": textLanguageFrom,
+            "text": answer
+            }).result[0]
+
+    def getTags(self, translation):
+        algo = client.algo('nlp/AutoTag/1.0.1')
+        return algo.pipe(translation).result
+
+    def getFind(self, tag):
+        find = {}
+        find["tags"] = {}
+        find["tags"]["$all"] = []
+        find["tags"]["$all"].append(tag)
+
+        return find
 
     def registerDbAndReturnNotReply(self, answer, translation, tags):
         _id = self.mongo["dev"]["conversations"].insert_one({
